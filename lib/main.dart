ac17e4426d39e7f343d839947c9d47f1022834ee
amira_nasser <amira20nasser@gmail.com>
2025-03-19 00:53:51 +0200

    ✨ feat(home view): add text theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:clean_archi_bookly/constants.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'features/splash/persentation/views/splash_view.dart';
 
 void main() {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       builder: (context) => const BooklyApp(),
     ),
   );
@@ -16,7 +17,6 @@ void main() {
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -24,7 +24,10 @@ class BooklyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
     );
   }
 }
