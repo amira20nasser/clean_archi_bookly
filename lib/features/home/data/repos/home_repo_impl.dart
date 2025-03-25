646076afb14c5e1e33c510cec78818a74245f103
amira_nasser <amira20nasser@gmail.com>
2025-03-25 04:18:00 +0200

    ✨ feat(HomeView): Implement failure and home repo

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,48 @@
+import 'package:clean_archi_bookly/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_archi_bookly/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import '../../../../core/errors/failure.dart';
+import '../../domain/entities/book_entity.dart';
+import '../../domain/repos/home_repo.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isEmpty) {
+        books = await homeRemoteDataSource.fetchFeaturedBooks();
+      }
+      return right(books);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDiorError(e));
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      var books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isEmpty) {
+        books = await homeRemoteDataSource.fetchNewestBooks();
+      }
+      return right(books);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDiorError(e));
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,18 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
+  await Hive.openBox(kNewestBox);
+
   runApp(
     DevicePreview(
       enabled: true,
       builder: (context) => const BooklyApp(),
     ),
   );
-  await Hive.initFlutter();
-  Hive.registerAdapter(BookEntityAdapter());
-  await Hive.openBox(kFeaturedBox);
-    await Hive.openBox(kNewestBox);
-
+  
 }
 
 class BooklyApp extends StatelessWidget {
